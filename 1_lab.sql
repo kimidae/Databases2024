d8641dd39258e914e2340c4f907b534867d6aa9d
kimidae <a_bratkova@kbtu.kz>
2024-09-19 22:02:39 +0500

    1commit

---
+++ 1_lab.sql
@@ -0,0 +1,31 @@
+CREATE
+DATABASE lab1;
+CREATE TABLE clients
+(
+    client_id   SERIAL PRIMARY KEY,
+    first_name  VARCHAR(60),
+    last_name   VARCHAR(60),
+    email       VARCHAR(100),
+    date_joined DATE
+);
+
+ALTER TABLE clients
+    ADD COLUMN status INT;
+
+ALTER TABLE clients
+ALTER
+COLUMN status TYPE BOOLEAN USING status::BOOLEAN;
+
+ALTER TABLE clients
+    ALTER COLUMN status SET DEFAULT TRUE;
+
+CREATE TABLE orders
+(
+    order_id   SERIAL PRIMARY KEY,
+    order_name VARCHAR(100),
+    client_id  INT REFERENCES clients (client_id)
+);
+
+DROP TABLE orders;
+DROP
+DATABASE lab1;
